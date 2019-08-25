791effaa035354b6392b84962dc4a8794257ff28
Victorien Marette <VictorienMarette.Pro@gmail.com>
2019-08-25 16:56:43 +0200

    nv

--- includes/ajax/sugestion.php
+++ includes/ajax/sugestion.php
@@ -1,20 +1,22 @@
 <?php
 if (isset($_POST["recherche"])) {
 	include_once("../background/dph.php");
-
 	$recherche = mysqli_real_escape_string($conn, $_POST["recherche"]);
 
-	$nombre_resultat = 5;
+	if (empty($recherche) != " ") {
 
-	$sql = "SELECT * FROM formations WHERE formation_nom LIKE '$recherche%' AND formation_public='1' ORDER BY formation_nom";
-	$result = mysqli_query($conn, $sql);
-	$resultCheck = mysqli_num_rows($result);
+		$nombre_resultat = 5;
 
-	if ($resultCheck>0) {
-		$nb_proposition = 0;
-		while ($row = mysqli_fetch_assoc($result) and $nb_proposition < $nombre_resultat) {
-			$nb_proposition = $nb_proposition + 1;
-			echo '<option>'.$row["formation_nom"].'</option>';
+		$sql = "SELECT * FROM formations WHERE formation_nom LIKE '$recherche%' AND formation_public='1' ORDER BY formation_nom";
+		$result = mysqli_query($conn, $sql);
+		$resultCheck = mysqli_num_rows($result);
+
+		if ($resultCheck>0) {
+			$nb_proposition = 0;
+			while ($row = mysqli_fetch_assoc($result) and $nb_proposition < $nombre_resultat) {
+				$nb_proposition = $nb_proposition + 1;
+				echo '<option>'.$row["formation_nom"].'</option>';
+			}
 		}
 	}
 }
