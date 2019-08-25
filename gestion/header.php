3a699c947de1dd7732f69f35522ff158999ad5fc
Victorien Marette <VictorienMarette.Pro@gmail.com>
2019-08-25 12:29:25 +0200

    debut gestion

---
+++ gestion/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!--<title></title>-->
+</head>
+<body>
+
+  <nav id="nav-test">
+
+  <div class="menu-tel">
+    <i class="fa fa-times fa-2x" onclick="menutelfin()"></i>
+  <div class="one">
+    <div class="align-23">
+    <form class="form-search" method="GET" action="div-produit-recherche.php">
+      <input class="input-recherche-2" type="text" name="code" placeholder="Recherche"><button class="button-recherche-2" type="submit"><img class="image-search-2" src="img/blue-loupe.png" alt=""></button>
+    </form>
+    </div>
+    <li class="liste-tel"><a class="simple" href="#">Accueil</a></li>
+    <li class="liste-tel"><a class="simple" href="#">Livre</a></li>
+  </div>
+  </div>
+
+
+
+
+
+
+
+
+
+<div class="menu-ordi">
+
+  <p class="titre-site">Clic<span class="logo">&</span>Book.fr</p>  
+    <div class="balle"><i class="fa fa-bars fa-2x" onclick="menutel()"></i></div>
+  <div class="menu-sans-titre">
+    <ul>
+      <li><a class="simple" href="index.php">Formations</a></li>
+      <li><a class="trou js-scrollTo" href="#matière-container">Livres<img class="icon-arrow" src="img/sort-down.png"></a>
+    <div class="container">
+      <div class="triangle">
+        
+      </div>
+    </div>
+  </li>
+  <li><a class="simple" href="#"></a></li>
+ <div class="align-2">
+    <form class="form-search" method="GET" action="div-produit-recherche.php">
+      <input class="input-recherche-2" type="text" name="recherche" placeholder="Recherche"><button class="button-recherche-2" type="submit" name="submit"><img class="image-search-2" src="img/blue-loupe.png" alt=""></button>
+    </form>
+  </div>
+
+</div>
+
+</li>
+
+    </ul>
+  </div>
+</div>
+  </nav>
---
+++ gestion/index.php
@@ -0,0 +1,9 @@
+<?php
+	include_once("header.php");
+?>
+<header class="v-header container-1">
+	<p>pute</p>
+</header>
+
+</body>
+</html>
